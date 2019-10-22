93974c6dff6d1faddeee0f85976ad55b8751e7cc
Ping Huang <ping.huang@waiwhanau.com>
2019-10-23 10:04:42 +1300

    Add project files.

---
+++ CredentialsFactory/Class1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredentialsFactory
+{
+    public enum SharePointAuthentication
+    {
+        SharePointOnline,
+        SharePointActiveDirectory
+    };
+
+    public class SharepointCredentials
+    {
+        public static ICredentials CreateCredentials(string UserName, string Password, SharePointAuthentication authticationType)
+        {
+            ICredentials result = null;
+
+            switch (authticationType)
+            {
+                case SharePointAuthentication.SharePointActiveDirectory:
+                    result = new NetworkCredential(UserName, Password);
+                    break;
+                case SharePointAuthentication.SharePointOnline:
+                    SecureString securePassword = new SecureString();
+                    foreach (char c in Password)
+                    {
+                        securePassword.AppendChar(c);
+                    }
+                    result = new SharePointOnlineCredentials(UserName, securePassword);
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
